a570657db47881b6b1427ef151816f0b1aaea012
Joy Muthoni Malanga <joyalphamuthoni@gmail.com>
2022-08-16 15:30:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = [ 
+    "Milo",
+    "Otis",
+    "Garfield"
+];
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+function appendCat(name){
+    const cat =[...cats, "Broom"];
+    return cat;
+  
+}
+function prependCat() {
+    const cat =["Arnold", ...cats];
+    return cat;
+}
+function removeLastCat() {
+    const cat =cats.slice(0,cats.length-1);
+    return cat;
+}
+function removeFirstCat() {
+    const cat =cats.slice(1);
+    return cat;
+}
+
+
+
+    
+
